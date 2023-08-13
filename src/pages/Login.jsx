fc796538481cc658a200248b7a8dc29f0a31f1a2
ndoelger <doelgern@gmail.com>
2023-08-13 15:54:27 -0400

    ready for deployment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,9 +24,14 @@ const StyledLoginButton = styled.a`
   }
 `;
 
+const LOGIN_URI =
+  process.env.NODE_ENV !== 'production'
+    ? 'http://localhost:8888/login'
+    : 'https://yourspotifystats-ef3f5ae7a1b3.herokuapp.com/login';
+
 const Login = () => (
   <StyledLoginContainer>
-    <StyledLoginButton href="http://localhost:8888/login">
+    <StyledLoginButton href={LOGIN_URI}>
       Log in to Spotify
     </StyledLoginButton>
   </StyledLoginContainer>
